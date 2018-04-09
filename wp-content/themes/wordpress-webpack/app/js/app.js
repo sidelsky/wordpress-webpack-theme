8b2a7d7a8affb22d5273d56d0bc83cde822b91b8
Errol Sidelsky <errol@squie.com>
2018-04-09 09:43:44 +0100

    Vimeo player working

--- wp-content/themes/wordpress-webpack/app/js/app.js
+++ wp-content/themes/wordpress-webpack/app/js/app.js
@@ -1,11 +1,13 @@
-/*jslint node: true */
+/* global require */
+/* global window */
+/* global site_data */
 /* jshint -W097 */
 
-//"use strict";
+"use strict";
 
-import { createLogger } from './scripts/utils';
-import { svgSprite } from './scripts/svg-sprite';
-import init_vimeo from './scripts/video';
+import { createLogger } from "./scripts/utils";
+import { svgSprite } from "./scripts/svg-sprite";
+import init_video from "./scripts/video";
 
 /**
  * Vendor scripts
@@ -15,30 +17,30 @@ const log = createLogger("app");
 log.setLevel(log.DEBUG);
 
 function init() {
-    log.info("Init called");
+   log.info("Init called");
 }
 
 //init();
 
 window.enhance_website = function() {
-    log.debug("window.enhance_website", document.readyState);
+   log.debug("window.enhance_website", document.readyState);
 
-    if (document.readyState === "complete") {
-        log.info("Complete");
-        init();
-    } else {
-        document.onreadystatechange = function() {
-            log.debug("document.onreadystatechange", document.readyState);
-            if (
-                document.readyState === "complete" ||
-                document.readyState === "interactive"
-            ) {
-                init();
-            } else {
-                document.onreadystatechange = null;
-            }
-        };
-    }
+   if (document.readyState === "complete") {
+      log.info("Complete");
+      init();
+   } else {
+      document.onreadystatechange = function() {
+         log.debug("document.onreadystatechange", document.readyState);
+         if (
+            document.readyState === "complete" ||
+            document.readyState === "interactive"
+         ) {
+            init();
+         } else {
+            document.onreadystatechange = null;
+         }
+      };
+   }
 };
 
 enhance_website();
